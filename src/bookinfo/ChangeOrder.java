d69ef8f344d37aadf9a7e038a9b13848660757ed
fyc1007261 <fyc1007261@sjtu.edu.cn>
2018-04-24 00:20:10 +0800

    cart update

--- src/bookinfo/ChangeOrder.java
+++ src/bookinfo/ChangeOrder.java
@@ -1,26 +1,24 @@
 package bookinfo;
 
 import database.BookEntity;
+import database.CustomerEntity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import database.OrderformEntity;
-import org.hibernate.criterion.Order;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.print.Book;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.OptionalDouble;
 
-@WebServlet("/purchase/fetch_cart")
-public class FetchCart extends HttpServlet {
+@WebServlet("/purchase/change_amount")
+public class ChangeOrder extends HttpServlet {
 
     private static final SessionFactory sessionFactory;
 
@@ -34,7 +32,6 @@ public class FetchCart extends HttpServlet {
     public static Session getSession() {
         return sessionFactory.openSession();
     }
-
 
     private String message;
 
@@ -58,25 +55,21 @@ public class FetchCart extends HttpServlet {
             resp.setContentType("text/html");
             PrintWriter out = resp.getWriter();
 
-            List<OrderformEntity> orders = session.createSQLQuery("select * from orderform where username=\""+usn +"\"")
-                    .addEntity(OrderformEntity.class)
-                    .list();
-            int len = orders.size();
-            StringBuffer buf = new StringBuffer("[");
-
-            for (int i=0; i<len; i++){
-                OrderformEntity order = orders.get(i);
-                BookEntity book = order.getBookByBookId();
-                buf.append(
-                        "{\"Book_name\" : \"" + book.getBookName() +
-                                "\", \"Order_id\" : \"" +order.getOrderId() +
-                                "\", \"Book_id\" : \"" +book.getBookId() +
-                                "\", \"Author\" : \"" +book.getAuthor() +
-                                "\", \"Price\" : \"" +book.getPrice() * order.getAmount() +
-                                "\", \"Amount\" : \""+ order.getAmount() +"\"}");
-                buf.append(i==len-1?"]":",");
+            String order_id =req.getParameter("order_id");
+            int new_num = Integer.parseInt(req.getParameter("new_amount"));
+            if(new_num<=0){
+                OrderformEntity order = session.get(OrderformEntity.class, order_id);
+                session.delete(order);
+                transaction.commit();
+                message = "Succeed";
             }
-            message = buf.toString();
+            else{
+                OrderformEntity order = session.get(OrderformEntity.class, order_id);
+                order.setAmount(new_num);
+                transaction.commit();
+                message = "Succeed";
+            }
+
             out.print(message);
         }
         catch (Exception e) {
