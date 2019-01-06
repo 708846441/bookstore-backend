32c8ca434e754ef3ea22a49ab88dfdb0bb1bf64b
708846441 <708846441@qq.com>
2019-01-06 14:29:39 +0800

    used for ci

--- src/main/java/com/yifan/bookstore/BookstoreApplication.java
+++ src/main/java/com/yifan/bookstore/BookstoreApplication.java
@@ -7,22 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 import java.util.List;
 
 @RestController
 @SpringBootApplication
-public class BookstoreApplication extends SpringBootServletInitializer{
+public class BookstoreApplication {
 	@Autowired
 	BookRepository bookRepository;
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return builder.sources(BookstoreApplication.class);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
