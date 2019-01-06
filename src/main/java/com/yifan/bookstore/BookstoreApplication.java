2c24569cc3fc5245988d62dd668ff8d2c56ab34d
Breezeeee <S-J-T-U-j-f-x@sjtu.edu.cn>
2019-01-06 15:53:52 +0800

    fit tomcat

--- src/main/java/com/yifan/bookstore/BookstoreApplication.java
+++ src/main/java/com/yifan/bookstore/BookstoreApplication.java
@@ -5,6 +5,8 @@ import com.yifan.bookstore.database.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +15,17 @@ import java.util.List;
 
 @RestController
 @SpringBootApplication
-public class BookstoreApplication {
-	@Autowired
-	BookRepository bookRepository;
+public class BookstoreApplication extends SpringBootServletInitializer {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+	    return builder.sources(BookstoreApplication.class);
+    }
+
+
 }
